6121a7b5e3da9f781965ddcf1d6f918b59622a01
Ariba Khan <akhan41@binghamton.edu>
2020-08-10 16:27:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,6 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].count do
       p src[row_index][element_index]
-    end
       element_index += 1
     end
     new_array << join(" ")
